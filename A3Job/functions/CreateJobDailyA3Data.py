159904a026bbdbe467de59865a061a886e0a466b
Katherine Sandoval <kate_sandoval27@hotmail.com>
2021-10-20 10:55:53 -0400

    Adds new states and corresponding lambdas ans sfn def

--- A3Job/functions/CreateJobDailyA3Data.py
+++ A3Job/functions/CreateJobDailyA3Data.py
@@ -5,13 +5,14 @@ import json
     This function would create the final set of data that is going to be ingested to Data Lake
 
     1. Read from JobDailyDelta S3 Bucket <Date - business_date input>/transformed-delta-data/[]
-    2. Create necessary format for data lake and save the results to JobDailyA3Data with a folder with the inout business_date
+    2. Create necessary format for data lake and save the results to JobDailyA3Data with a folder with the input business_date
+    3. Return the size of the transformed data
 """
 
 def lambda_handler(event, context):
 
     return {
-        'key': 'value'
+        'total_documents_processed': 1000
     }
 
 
--- A3Job/functions/distributor/ProcessPayloadForRecursion.py
+++ A3Job/functions/distributor/ProcessPayloadForRecursion.py
@@ -1,5 +1,16 @@
 """
     This function split batches if required
+
+    "delta_docs_to_run": [
+        {
+          "delta_doc_number": 0,
+          "lex_id": "69988902-c227-41a0-824c-7a3996d799a0"
+        },
+        {
+          "delta_doc_number": 40,
+          "lex_id": "0d2fcf11-c41a-4bf6-ac07-c6eb7d2994ef"
+        },
+    ]
 """
 
 def lambda_handler(event, context):
@@ -8,20 +19,22 @@ def lambda_handler(event, context):
     delta_docs_to_run = event.get('delta_docs_to_run')
     print("delta_docs_to_run {}".format(delta_docs_to_run))
 
-    max_parallel_executions = event.get("max_parallel_executions")
+    # hardcoding for now
+    max_parallel_executions = 40 # event.get("max_parallel_executions")
 
     return_value = event
 
     # If the list includes more than the maximum number of max_parallel_executions, then split into batches.
     # Otherwise return the data as is.
-    if len(delta_docs_to_run) > max_parallel_executions:
+    if len(delta_docs_to_run) > max_parallel_executions: # e.g 558 > 40
         delta_docs_batches = []
         current_batch = {"batch_id": 0, "delta_docs_to_run": []}
         added = 0
         for doc in delta_docs_to_run:
             current_batch['delta_docs_to_run'].append(doc)
-            added = 0
+            added = 0 # re-set for after the below if has run
             if len(current_batch['delta_docs_to_run']) >= max_parallel_executions:
+                # if we have appended 40, then create a new batch and replace the current batch
                 delta_docs_batches.append(current_batch)
                 new_batch_id = current_batch['batch_id'] + 1
                 current_batch = {"batch_id": new_batch_id, "delta_docs_to_run": []}
